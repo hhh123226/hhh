9b52b75efe6e169830a48969ebe6be2d33f97f9f
hhh123226 <2543949269@qq.com@example.com>
2026-05-07 23:58:26 +0800

    版本2：增加键盘输入功能

--- ex01/hello.c
+++ ex01/hello.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+
 int main() {
+    char input[100];  // 定义字符数组存储输入内容
+
     printf("Hello, World!\n");
+
+    // 键盘输入功能
+    printf("请输入一段内容：");
+    scanf("%s", input);  // 读取用户输入的字符串
+
+    // 回显输入内容
+    printf("你输入的内容是：%s\n", input);
+
     return 0;
 }
+
