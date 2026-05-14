44fe8ae6ef3ef41004b5b9f0020a176c65448ffb
hhh123226 <2543949269@qq.com@example.com>
2026-05-14 17:11:43 +0800

    lab03: 调整呼吸速度，延长渐变时间

--- lab03/lab03.ino/lab03.ino.ino
+++ lab03/lab03.ino/lab03.ino.ino
@@ -11,12 +11,12 @@ void loop() {
   
   for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){
     ledcWrite(ledPin, dutyCycle);
-    delay(10);
+    delay(15);
   }
   
   for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
     ledcWrite(ledPin, dutyCycle);
-    delay(10);
+    delay(15);
   }
   Serial.println("Breathing cycle completed");
 }
