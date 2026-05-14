b868868f8ecc97ec047480e2127bd72cdf2ad89e
hhh123226 <2543949269@qq.com@example.com>
2026-05-14 17:19:35 +0800

    ex02: 添加函数注释，说明millis原理

--- ex02/ex02_1hz.ino/ex02_1hz.ino.ino
+++ ex02/ex02_1hz.ino/ex02_1hz.ino.ino
@@ -1,6 +1,7 @@
+// ex02 millis() 实现1Hz LED闪烁（ESP32 GPIO2）
 const int ledPin = 2;
 unsigned long previousMillis = 0;
-const long interval = 500;   
+const long interval = 500;   // 500ms切换一次，1Hz频率
 bool ledState = LOW;
 
 void setup() {
@@ -10,7 +11,7 @@ void setup() {
 
 void loop() {
   unsigned long currentMillis = millis();
-  
+  // 非阻塞判断时间
   if(currentMillis - previousMillis >= interval){
     previousMillis = currentMillis;
     ledState = !ledState;
