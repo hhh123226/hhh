c9fbcd0c6093ef8c908b019065de1f99dec1b567
hhh123226 <2543949269@qq.com@example.com>
2026-05-14 17:13:29 +0800

    lab03: 优化代码结构，完善注释

--- lab03/lab03.ino/lab03.ino.ino
+++ lab03/lab03.ino/lab03.ino.ino
@@ -1,3 +1,4 @@
+//ESP32 实验3 PWM呼吸灯
 const int ledPin = 2;
 const int freq = 5000;
 const int resolution = 8;
@@ -8,12 +9,12 @@ void setup() {
 }
 
 void loop() {
-  
+  //逐渐变亮
   for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){
     ledcWrite(ledPin, dutyCycle);
     delay(15);
   }
-  
+  //逐渐变暗
   for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
     ledcWrite(ledPin, dutyCycle);
     delay(15);
